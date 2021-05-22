32155476ca884c58131550953ba61580f083f238
Katerina Davidova <kat@moucha.cc>
2021-05-22 15:42:03 +0200

    pridej hotovy project 14

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
 import { render } from 'react-dom';
+import React, { useState } from 'react';
 import './style.css';
 
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
-  </div>
-);
+const App = () => {
+  const [scoreUp, setScoreUp] = useState(0);
+  const [scoreDown, setScoreDown] = useState(0);
+
+  const handleClickUp = () => {
+    setScoreUp(scoreUp + 1);
+  };
+
+  const handleClickDown = () => {
+    setScoreDown(scoreDown + 1);
+  };
+
+  return (
+    <div className="container">
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img
+              className="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
+            <p className="user-name">Neroxx</p>
+          </div>
+
+          <p className="joke__text">
+            The secret service isn't allowed to yell "Get down!" anymore when
+            the president is about to be attacked. Now they have to yell
+            "Donald, duck!"
+          </p>
+        </div>
+        <div className="joke__likes">
+          <button
+            id="btn-up"
+            className="btn-like btn-like--up"
+            onClick={handleClickUp}
+          ></button>
+
+          <span id="likes-up" className="likes-count likes-count--up">
+            {scoreUp}
+          </span>
+          <button
+            id="btn-down"
+            className="btn-like btn-like--down"
+            onClick={handleClickDown}
+          ></button>
+
+          <span id="likes-down" className="likes-count likes-count--down">
+            {scoreDown}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
